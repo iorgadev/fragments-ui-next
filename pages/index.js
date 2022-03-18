1baffb0213b7da26c7d6d4be93ef1b059d930a6b
iorgadev <2624432+iorgadev@users.noreply.github.com>
2022-03-18 14:30:27 -0400

    clean up index

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,9 @@
-import { useEffect } from "react";
 import { useAtom } from "jotai";
 import { userAtom } from "./_app";
 import Loading from "../components/Loading";
 
 const Home = () => {
   const [user, setUser] = useAtom(userAtom);
-
-  useEffect(() => {
-    console.log("Home.js - User: ", user);
-  }, []);
 
   if (!user || !user.username) {
     return <Loading />;
@@ -18,7 +13,12 @@ const Home = () => {
     <div>
       Hello, {user.username}
       <div>
-        <button className="bg-red-500">Logout</button>
+        <button
+          className="bg-red-500"
+          onClick={() => setUser((prevUser) => (prevUser = {}))}
+        >
+          Logout
+        </button>
       </div>
     </div>
   );
