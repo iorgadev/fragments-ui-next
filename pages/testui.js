9e721b8419b37f0c788db5f115705d9363b1dd1c
iorgadev <2624432+iorgadev@users.noreply.github.com>
2022-03-20 19:12:45 -0400

    test page to see components that are wip

---
+++ pages/testui.js
@@ -0,0 +1,14 @@
+import React from "react";
+import DragDropFragments from "../components/User/DragDropFragments";
+import LoggedInAs from "../components/User/LoggedInAs";
+
+function TestUIPage() {
+  return (
+    <div className="main">
+      {/* <LoggedInAs /> */}
+      <DragDropFragments />
+    </div>
+  );
+}
+
+export default TestUIPage;
