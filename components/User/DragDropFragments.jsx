2110b4644f79ce177ac7999aadf089bfbf4ad5a1
iorgadev <2624432+iorgadev@users.noreply.github.com>
2022-03-22 19:34:08 -0400

    wip drag drop Fragments to create new fragments

--- components/User/DragDropFragments.jsx
+++ components/User/DragDropFragments.jsx
@@ -1,13 +1,16 @@
 import { useState, useEffect } from "react";
-import { useAtom, atom } from "jotai";
+import { useAtom } from "jotai";
 import { userAtom } from "../../pages/_app";
+import { getFileType, getExtensionMimeType } from "../../utils/fragmentTypes";
 
 function DragDropFragments() {
   const [user] = useAtom(userAtom);
   const [files, setFiles] = useState([]);
+  const [uploadedFiles, setUploadedFiles] = useState([]);
   const [dropError, setDropError] = useState(false);
   const [isDragging, setIsDragging] = useState(false);
   const [progress, setProgress] = useState(0);
+  const [filesDropped, setFilesDropped] = useState(false);
 
   const dragOver = (e) => {
     e.preventDefault();
@@ -24,17 +27,6 @@ function DragDropFragments() {
     setIsDragging(false);
   };
 
-  const fileDrop = (e) => {
-    e.preventDefault();
-    setIsDragging(false);
-    const files = e.dataTransfer.files;
-    console.log(files);
-    setFiles((prevFiles) => (prevFiles = []));
-    for (let file of files) {
-      setFiles((prevFiles) => [...prevFiles, file.name]);
-    }
-  };
-
   const wrongDrop = (e) => {
     e.preventDefault();
     if (e.target.id != "dropFileContainer") setDropError(true);
@@ -43,18 +35,20 @@ function DragDropFragments() {
   const handleDrop = async (e) => {
     e.preventDefault();
     e.stopPropagation();
-    // setFilesDropped(true);
+    setIsDragging((prev) => false);
+    setFilesDropped(true);
     // setLoading(true);
-    const files = e.dataTransfer.files;
-    const formData = new FormData();
-    for (let i = 0; i < files.length; i++) {
-      formData.append("files", files[i]);
+    setFiles([]);
+    var droppedFiles = e.dataTransfer.files;
+    for (var i = 0; i < droppedFiles.length; i++) {
       const response = await fetch(
         `${process.env.NEXT_PUBLIC_API_URL}/v1/fragments`,
         {
           method: "POST",
           headers: {
-            "Content-Type": files[i].type,
+            "Content-Type": getExtensionMimeType(
+              getFileType(droppedFiles[i].name)
+            ),
             Authorization: `Bearer ${user.signInUserSession.idToken.jwtToken}`,
           },
           // onUploadProgress: (progressEvent) => {
@@ -62,44 +56,38 @@ function DragDropFragments() {
           //     Math.round((progressEvent.loaded * 100) / progressEvent.total)
           //   );
           // },
-          // body: formData,
-          body: Buffer.from(files[i].name, "base64"),
+          body: droppedFiles[i],
         }
       );
-      // clear formData
-      formData.delete("files");
-
       const data = await response.json();
-      console.log("Data Response: ", data);
+      setFiles((prev) => {
+        return [...prev, droppedFiles[i].name];
+      });
+      // console.log("File: ", uploadedFiles[i].name);
+      // (function (uploadedFiles) {
+      //   setTimeout(function () {
+      //     console.log("File: ", uploadedFiles[i].name);
+      //     // setProgress((prev) => prev + 1);
+      //   }, 500);
+      // })(uploadedFiles);
+
+      // currentFile = droppedFiles[i].name;
+      // setTimeout(() => {
+      //   console.log("File: ", droppedFiles[i].name);
+      //   // uploaded.push(droppedFiles[i].name);
+      //   console.log("File: ", uploaded);
+      //   setFiles((prev) => [...prev, currentFile]);
+      // }, i * 1000);
     }
-    // const response = await fetch(
-    //   `${process.env.NEXT_PUBLIC_API_URL}/v1/fragments`,
-    //   {
-    //     method: "POST",
-    //     headers: {
-    //       Authorization: `Bearer ${user.signInUserSession.idToken.jwtToken}`,
-    //     },
-    //     onUploadProgress: (progressEvent) => {
-    //       setProgress(
-    //         Math.round((progressEvent.loaded * 100) / progressEvent.total)
-    //       );
-    //     },
-    //     body: formData,
-    //   }
-    // );
-    // const data = await response.json();
-    // console.log("handleDrop() data: ", data);
-    // setLoading(false);
-    // setFragments(data.fragments);
   };
 
-  useEffect(() => {
-    console.log("Files: ", files);
-  }, [files]);
+  // useEffect(() => {
+  //   console.log("Files: ", files);
+  // }, [files]);
 
   return (
     <div
-      className={`flex items-center justify-center w-full h-full border-4 border-orange-600 transition-all duration-500 rounded-xl ${
+      className={`flex flex-col items-center justify-center w-full h-full border-4 border-orange-600 transition-all duration-500 rounded-xl ${
         isDragging ? "" : "border-dashed"
       }`}
       onDragOver={dragOver}
@@ -107,16 +95,19 @@ function DragDropFragments() {
       onDragLeave={dragLeave}
       onDrop={handleDrop}
     >
-      {files ? (
+      {files.length === 0 ? (
         <span className="text-lg text-orange-300 pointer-events-none">
           Drag and drop files here to create Fragments
         </span>
       ) : (
-        files.map((file) => {
+        files.map((file, i) => {
           return (
-            <span className="text-lg text-orange-300 pointer-events-none">
+            <div
+              key={i}
+              className="text-lg text-orange-300 pointer-events-none"
+            >
               {file}
-            </span>
+            </div>
           );
         })
       )}
