d64f0dcaa0ecd1ae18c4de49517fa49245dfd236
iorgadev <2624432+iorgadev@users.noreply.github.com>
2022-03-22 22:30:48 -0400

    update Loading component

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Loading() {
-  return <div>Loading</div>;
+  return (
+    <div className="relative flex items-center justify-center h-full col-span-6">
+      <div className="loader">
+        <div className="loader__inner"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Loading;
