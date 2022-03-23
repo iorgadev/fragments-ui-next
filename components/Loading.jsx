90a52f726faa7c66800ef79e306343f13dab9901
iorgadev <2624432+iorgadev@users.noreply.github.com>
2022-03-23 15:26:59 -0400

    made div absolute to fill up full width/height

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Loading() {
   return (
-    <div className="relative flex items-center justify-center h-full col-span-6">
+    <div className="absolute flex items-center justify-center w-full h-full col-span-6">
       <div className="loader">
         <div className="loader__inner"></div>
       </div>
