8673eb05031454078ed7879f09231c8de74a641b
iorgadev <2624432+iorgadev@users.noreply.github.com>
2022-03-25 11:26:18 -0400

    fix Loading component size/position

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Loading() {
   return (
-    <div className="absolute flex items-center justify-center w-full h-full col-span-6">
+    <div className="absolute top-0 left-0 flex items-center justify-center w-full h-full col-span-6">
       <div className="loader">
         <div className="loader__inner"></div>
       </div>
